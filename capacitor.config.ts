9034139952d143aa17d58ebfeb23c1ebe6f2c8b4
Jaswin Jazz <jaswinjazz25@gmail.com>
2026-08-12 23:02:51 +0530

    Bump service worker cache version to v2 for logo update

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.parkerz.eleven',
-  appName: '11 Parkerz',
-  webDir: 'dist'
-};
-
-export default config;
